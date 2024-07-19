489ad623f79a1f775a14ce21c136e8fc936bd99d
Asafbishoy <91992450+Asafbishoy@users.noreply.github.com>
2024-07-18 22:19:44 -0500

    added erd and insert data's database in schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,8 +6,14 @@ CREATE TABLE Client (
 );
 CREATE TABLE Account (
     account_id INT PRIMARY KEY AUTO_INCREMENT,
-    account_type Account_Type ,
-    account_balance DOUBLE PRECISION not null,
+    account_type Account_Type default "CHECKING" ,
+    account_balance DOUBLE PRECISION not null check (account_balance>0),
     primary_client_id int not null,
-    FOREIGN KEY (primary_client_id) REFERENCES Client(client_id)
+    FOREIGN KEY (primary_client_id) REFERENCES Client(client_id) ON DELETE CASCADE
 );
+insert into client(client_name, client_email, client_password) values('john','john@gmail.com','5691');
+insert into client(client_name, client_email, client_password) values('jimmy','jimmy@gmail.com','5691');
+insert into client(client_name, client_email, client_password) values('tom','tom@gmail.com','5691');
+insert into account(account_type,account_balance,primary_client_id) values('CHECKING'::public."account_type",5000,1);
+insert into account(account_type,account_balance,primary_client_id) values('SAVING'::public."account_type",10000,2);
+insert into account(account_type,account_balance,primary_client_id) values('CHECKING'::public."account_type",50000,3);
